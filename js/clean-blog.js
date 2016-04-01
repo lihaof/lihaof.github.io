1a238b0edec1e408bf7a280469a012f09636db30
lihaof <vonlion.f@gmail.com>
2016-04-01 19:32:56 +0800

    change name

--- js/clean-blog.js
+++ js/clean-blog.js
@@ -5,7 +5,7 @@
  */
 
 // Tooltip Init
-// Unuse by zyt since V1.6: Titles now display by default so there is no need for tooltip
+// Unuse by von since V1.6: Titles now display by default so there is no need for tooltip
 // $(function() {
 //     $("[data-toggle='tooltip']").tooltip();
 // });
@@ -13,7 +13,7 @@
 
 // make all images responsive
 /* 
- * Unuse by zyt
+ * Unuse by von
  * actually only Portfolio-Pages can't use it and only post-img need it.
  * so I modify the _layout/post and CSS to make post-img responsive!
  */
